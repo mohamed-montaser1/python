e67eb429f07ddb0cc7413c1b8bd831282d0bfab2
mohamed montaser <mastrocoding@gmail.com or muslim.mohamed.montaser@gmail.com>
2023-12-08 14:00:01 +0200

    comments.py

---
+++ comments.py
@@ -0,0 +1,18 @@
+# This is just a single line comment
+# You can not create a multiple line comment in python
+# """ => means string but you did not assign it to a variable so it works just like a multiple line comment
+# you should use # instead of """ to create a comment like this
+
+# my
+# name
+# is
+# mohamed montaser
+
+# when you create a comment in your code ?
+# If you want to describe why you choose this way to do certain functionality
+# ! If you want to warn your team from change something in a code
+# If you want to describe an important function
+# If you want to comment a certain code
+
+# Bad example of using comments
+print("hello world") # this line of code will print "hello world" message
