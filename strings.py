851d75b7704aecaae521e353680397bdb67f0150
mohamed montaser <mastrocoding@gmail.com or muslim.mohamed.montaser@gmail.com>
2023-12-09 00:17:12 +0200

    strings.py

---
+++ strings.py
@@ -0,0 +1,22 @@
+# -------------------------
+# -------- Strings --------
+# -------------------------
+
+myStringOne = 'This is Single Quote'
+myStringTwo = "This is Double Quotes"
+myStringThree = "This is Double Quotes 'test'"
+
+print(myStringOne)
+print(myStringTwo)
+print(myStringThree)
+
+myStringFour = '''First
+Second 'test' "test"
+Third'''
+
+myStringFive = """First
+Second "test" 'test'
+Third"""
+
+print(myStringFour)
+print(myStringFive)
