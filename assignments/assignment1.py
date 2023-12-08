25e14f823c24b63efbfaf86c5b6b12d62c36c430
mohamed montaser <mastrocoding@gmail.com or muslim.mohamed.montaser@gmail.com>
2023-12-09 00:12:03 +0200

    assignments/assignment1.py

---
+++ assignments/assignment1.py
@@ -0,0 +1,15 @@
+name = "mohamed montaser"
+age = "15"
+country = "Egypt"
+
+print("Name: " + name)
+print("Age: " + age)
+print("Country " + country)
+
+msg = "Hello, My Name Is " + name + " And Iam " + age + " Years Old and I Live in " + country + "."
+
+print(msg)
+
+print(type(name))
+print(type(age))
+print(type(country))
