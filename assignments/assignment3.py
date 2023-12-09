801cb5b22b56fae9ebdba5158732f1eda28ef5b5
mohamed montaser <mastrocoding@gmail.com or muslim.mohamed.montaser@gmail.com>
2023-12-09 18:06:08 +0200

    assignment3.py

---
+++ assignments/assignment3.py
@@ -0,0 +1,20 @@
+print(type(1))
+print(type(2.0))
+print(type(2+4j))
+
+complexOne = 1+2j
+print(complexOne.imag)
+print(complexOne.real)
+
+num = 10
+print("{:.10f}".format(num))
+
+num = 159.650
+print(int(num))
+print(type(int(num)))
+
+print(100 - 115) # -15
+print(50 * 30) # 1500
+print(21 % 4) # 1
+print(110 / 11) # 10
+print(97 // 20) # 4
