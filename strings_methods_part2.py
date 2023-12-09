9b150ba39fb1ce3e1fa2ebc727a04af32b249dc3
mohamed montaser <mastrocoding@gmail.com or muslim.mohamed.montaser@gmail.com>
2023-12-09 10:19:16 +0200

    strings_methods_part2.py

--- strings_methods_part2.py
+++ strings_methods_part2.py
@@ -5,38 +5,38 @@
 # split() rsplit()
 
 a = "I Love Python and PHP"
-print(a.split())
+print(a.split()) # ['I', 'Love', 'Python', 'and', 'PHP']
 
 b = "I-Love-Python-and-PHP"
-print(b.split('-'))
+print(b.split('-')) # ['I', 'Love', 'Python', 'and', 'PHP']
 
 c = "I-Love-Python-and-PHP"
-print(b.split('-', 3)) 
+print(b.split('-', 3)) # ['I', 'Love', 'Python', 'and-PHP']
 
 d = "I-Love-Python-and-PHP"
-print(b.rsplit('-', 3))
+print(b.rsplit('-', 3)) # ['I-Love', 'Python', 'and', 'PHP']
 
-# center
+# center()
 
 e = "Mohamed"
 print(e.center(11)) # Spaces
 print(e.center(11, "#")) # Hashes
 print(e.center(15, "@")) # @
 
-# count()
+# count(Searched Word, Start, End)
+# it count how many the searched word appear
 
 f = "I Love Python and PHP Because PHP is Easy"
 print(f.count("PHP")) # 2 PHP words
 print(f.count("PHP", 0, 25)) # Only one PHP
-
 
 # swapcase()
 
 g = "I Love Python"
 h = "i lOVE pYTHON"
 
-print(g.swapcase())
-print(h.swapcase())
+print(g.swapcase()) # i lOVE pYTHON
+print(h.swapcase()) # I Love Python
 
 # startswith()
 
