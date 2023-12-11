85e32533d680ba7d232d5b5cf90bc8598e0b379c
mohamed montaser <mastrocoding@gmail.com or muslim.mohamed.montaser@gmail.com>
2023-12-11 13:29:29 +0200

    math-exam.py

--- mini-projects/math-exam.py
+++ mini-projects/math-exam.py
@@ -5,6 +5,7 @@ import random
 
 questions_length= int(input("How many question do you want? "))
 asked_questions = 1
+correct_questions = 0
 
 def main():
   question = question_generator()
@@ -14,9 +15,10 @@ def main():
   answer = input(f"what {num1} {operator} {num2} is? ")
 
   if answer.isdigit() or is_float(answer):
-    check_is_correct(num1, num2, operator, answer)
+    return check_is_correct(num1, num2, operator, answer)
   else:
     print("Please enter a valid number!")
+    return False
 
 def is_float(string):
   try:
@@ -38,10 +40,23 @@ def check_is_correct(num1, num2, operator, answer):
   answer = "{:.1f}".format(float(answer))
   if correct_answer.strip(" ") == answer.strip(" "):
     print("You are correct!")
+    return True
   else:
     print("Sorry your answer is wrong")
     print(f"Correct answer is: {correct_answer}")
+    return False
 
 while asked_questions <= questions_length:
-  main()
+  correct = main()
+  print(correct)
+  if correct:
+    correct_questions += 1
   asked_questions += 1
+
+while True:
+  if asked_questions > questions_length:
+    print("\n" + "".center(60, "#"))
+    print(f"\tcorrect answers: {correct_questions}")
+    print(f"\twrong answers: {questions_length - correct_questions}")
+    print("".center(60, "#"), "\n")
+    break
